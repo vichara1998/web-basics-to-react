c5d4ad8849e30f8875cf89aa859a04f1e989da9b
Vichara Tharkana <vichara0115@gmail.com>
2026-04-21 09:51:40 +0530

    Day 9

---
+++ D9/index.js
@@ -0,0 +1,25 @@
+console.log ("Hello")
+
+// Code Inject
+document.write ('Hello 2')
+document.write ("<h1> JAVASCRIPT</h1>")  
+
+//Alert
+// alert ("Welocme")
+
+const number =10.6
+
+const string ="Hi"
+
+const bool = true;
+
+const object ={name : "Bandara",age: 10}
+
+const array =[{name : "Bandara",age: 10},23,true,"Amal"]
+
+const a =5
+const b =6
+const muti =a*b
+console.log(muti);
+
+
